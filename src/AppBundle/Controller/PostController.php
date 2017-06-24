f19946633833816e4d0fb5909974c804d31d0495
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-24 11:33:13 +0200

    Action "show" pour récupérer un article via son identifiant, pour le moment l'article est crée dans le controller

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -2,9 +2,27 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
-class PostController extends Controller
-{
+class PostController extends Controller {
+
+    /**
+     * Renvoi un article par rapport à son identifiant
+     *
+     * @Route("/articles/{id}", name="articles_show")
+     */
+    public function showAction() {
+        $article = new Post();
+        $article
+            ->setTitle('Mon premier article')
+            ->setContent('Le contenu de mon premier article');
+        $data = $this->get('jms_serializer')->serialize($article, 'json');
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
 }
