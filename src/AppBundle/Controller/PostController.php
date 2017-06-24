51607195061a7b0fe82520dd68b1e56408ab3a5e
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-24 11:45:48 +0200

    Action "create" pour créer un article

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller {
@@ -23,6 +25,23 @@ class PostController extends Controller {
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
         return $response;
+    }
+
+    /**
+     * Reçoit les données d'un article crée et l'ajoute en base de données
+     *
+     * @param Request $request
+     * @Route("/articles", name="article_create")
+     * @Method({"POST"})
+     * @return Response
+     */
+    public function createAction(Request $request) {
+        $data = $request->getContent();
+        $post = $this->get('jms_serializer')->deserialize($data, 'AppBundle\Entity\Post', 'json');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($post);
+        $em->flush();
+        return new Response('Article crée', Response::HTTP_CREATED);
     }
 
 }
